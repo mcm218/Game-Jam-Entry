eb2a4def6d13348db5599a820faa68764e56415e
Michael Muniz <mcmuniz218@gmail.com>
2022-04-02 01:11:26 -0400

    Add some initial classes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+        public int sandResources = 10;
+
+        public int rockResources = 0;
+
+        public int seashellResouces = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     /// <summary>
+    /// How much sand the player has
+    /// </summary>
+    public int sandResources = 10;
+
+    /// <summary>
+    /// How many rocks the player has
+    /// </summary>
+    public int rockResources = 0;
+
+    /// <summary>
+    /// How many seashells the player has
+    /// </summary>
+    public int seashellResouces = 0;
+
+    /// <summary>
     /// The speed of the player
     /// </summary>
     [SerializeField, Range (0.1f, 10f)]
@@ -21,11 +36,20 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidBody;
 
     /// <summary>
+    /// The collider of the player
+    /// </summary>
+    private CircleCollider2D collider;
+
+    /// <summary>
     /// Start is called before the first frame update
     /// </summary>
     private void Start()
     {
+        // Getr the player's rigidbody so we can move them
         this.rigidBody = this.GetComponent<Rigidbody2D>();
+
+        // Get the player's collider so we can detect if they touch anything
+        this.collider = this.GetComponent<CircleCollider2D>();
     }
 
     /// <summary>
@@ -38,6 +62,25 @@ public class PlayerController : MonoBehaviour
 
         // Get the current vertical input
         this.movement.y = Input.GetAxisRaw("Vertical");
+
+        // Did the player left click?
+        if (Input.GetMouseButtonDown (0))
+        {
+            // Get the position of the mouse in the screen
+            Vector3 mousePos = Input.mousePosition;
+
+            // Convert the mouse position to a point in the world
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            // Cast a ray to detect if the player clicked an object with a collider
+            RaycastHit2D raycastHit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+            // Was anything clicked?
+            if (raycastHit)
+            {
+
+            }
+        }
     }
 
     /// <summary>
@@ -46,9 +89,9 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         // Move the player
-        // Movement handles the direction
-        // Speed handles how fast the player is moving
-        // Fixed Delta Time ensures movement is also based on the time since the last frame
+        // - Movement handles the direction
+        // - Speed handles how fast the player is moving
+        // - Fixed Delta Time ensures movement is constrained to the time since the last frame
         this.rigidBody.MovePosition(this.rigidBody.position + (this.movement * this.speed * Time.fixedDeltaTime));
     }
 }
