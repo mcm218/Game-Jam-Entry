c3bdbc16a93250193778f4872603df31cd804d0e
Michael Muniz <mcmuniz218@gmail.com>
2022-04-02 01:28:33 -0400

    Add some initial prefabs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,6 @@ namespace Assets.Scripts
 {
     public class GameController : MonoBehaviour
     {
-        public int sandResources = 10;
-
-        public int rockResources = 0;
-
-        public int seashellResouces = 0;
 
         // Use this for initialization
         void Start()
